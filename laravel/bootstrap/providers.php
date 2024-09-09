10409a9765f57d56977edc540bdaf5ce5a87fd4f
Renan Joppert <renanljp@gmail.com>
2024-09-09 10:39:51 -0300

    Instalando o Laravel Volt

--- laravel/bootstrap/providers.php
+++ laravel/bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
+    App\Providers\VoltServiceProvider::class,
 ];
