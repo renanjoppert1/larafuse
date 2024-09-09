d32ae2f8ae1b5ef4d993254157cc999a5ffeb077
Renan Joppert <renanljp@gmail.com>
2024-09-09 11:47:21 -0300

    Add debug bar

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -20,7 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $loader->alias('Debugbar', \Barryvdh\Debugbar\Facades\Debugbar::class);
     }
 
     /*
